ca3255dfccdb26ef4570f571ede6aa47905c38ee
baron <baron>
2005-11-24 19:16:30 +0000

    - added partial widgetset for fvat tab

--- tabfaktura.cpp
+++ tabfaktura.cpp
@@ -2,10 +2,15 @@
 #include "globals.h"
 #include "tabfaktura.h"
 
+#include <Box.h>
 #include <Button.h>
 #include <ListView.h>
+#include <Menu.h>
+#include <MenuField.h>
+#include <MenuItem.h>
 #include <ScrollView.h>
 #include <TabView.h>
+#include <TextControl.h>
 
 #include <stdio.h>
 
@@ -17,6 +22,10 @@ const uint32 BUT_RESTORE= 'TFBR';
 const uint32 BUT_SAVE	= 'TFBS';
 const uint32 DC			= 'TFDC';
 
+const uint32 CBUT		= 'TFCB';
+const uint32 MENUST		= 'TFMS';
+
+const char *stransportu[] = { "własny sprzedawcy", "własny odbiorcy", NULL };
 
 tabFaktura::tabFaktura(BTabView *tv, sqlite *db) : beFakTab(tv, db) {
 
@@ -59,6 +68,53 @@ tabFaktura::tabFaktura(BTabView *tv, sqlite *db) : beFakTab(tv, db) {
 	tbv2->AddTab(viewpozy, tabpozy);
 	tabpozy->SetLabel("Pozycje");
 //	views: 0,0,490,600
+	// box1
+	box1 = new BBox(BRect(10,10,300,180),"tf1box1");
+	box1->SetLabel("");
+	viewogol->AddChild(box1);
+	// box1-stuff
+	ogol[0] = new BTextControl(BRect(10,10,280,30), "tfd0", "Miejsce wyst.", NULL, new BMessage(DC));
+	ogol[1] = new BTextControl(BRect(10,40,280,60), "tfd1", "Wystawił", NULL, new BMessage(DC));
+	ogol[2] = new BTextControl(BRect(10,70,250,90), "tfd2", "Data wyst.", NULL, new BMessage(DC));
+	ogol[3] = new BTextControl(BRect(10,100,250,120), "tfd3", "Data sprzedaży", NULL, new BMessage(DC));
+	ogol[4] = new BTextControl(BRect(10,130,250,150), "tfd4", "Środek transp.", NULL, new BMessage(DC));
+	box1->AddChild(ogol[0]);
+	box1->AddChild(ogol[1]);
+	box1->AddChild(ogol[2]);
+	box1->AddChild(ogol[3]);
+	box1->AddChild(ogol[4]);
+	BMessage *msg;
+	msg = new BMessage(CBUT);
+	msg->AddInt32("_butnum", 0);
+	cbut[0] = new BButton(BRect(260,70,280,90), "tfcbut0", "+", msg);
+	cbut[1] = new BButton(BRect(260,100,280,120), "tfcbut1", "+", msg);
+	box1->AddChild(cbut[0]);
+	box1->AddChild(cbut[1]);
+	int i;
+	for (i=0;i<=4;i++)
+		ogol[i]->SetDivider(70);
+	// box1-menu
+	BMenu *menust = new BMenu("");
+//	BMessage *msg;
+	int j=0;
+	while (stransportu[j] != NULL) {
+		msg = new BMessage(MENUST); msg->AddString("_st",stransportu[j]);
+		menust->AddItem(new BMenuItem(stransportu[j], msg));
+		j++;
+	}
+	BMenuField *menustField = new BMenuField(BRect(260,130,280,150), "tfmfst", NULL, menust);
+	box1->AddChild(menustField);
+	// box2
+	box2 = new BBox(BRect(310,10,590,90),"tf1box2");
+	box2->SetLabel("Płatność");
+	viewogol->AddChild(box2);
+	// box2-stuff
+	// box3
+	box3 = new BBox(BRect(310,100,590,180),"tf1box3");
+	box3->SetLabel("Zaliczka");
+	viewogol->AddChild(box3);
+	// updateTab();
+	// RefreshIndexList();
 }
 
 tabFaktura::~tabFaktura() {
--- tabfaktura.h
+++ tabfaktura.h
@@ -5,6 +5,7 @@
 #include "befaktab.h"
 #include "fakdata.h"
 
+class BBox;
 class BButton;
 class BListView;
 class BTab;
@@ -21,10 +22,14 @@ class tabFaktura : public beFakTab {
 		BView *viewogol, *viewpozy;
 		BTab *tabogol, *tabpozy;
 		BTabView *tv2;
+		BBox *box1, *box2, *box3;
 
 		BButton *but_new, *but_del, *but_restore, *but_save;
 //		BButton *but_sell, *but_marza, *but_import;
+		BButton *cbut[5];
 		BListView *list;
+
+		BTextControl *ogol[11];
 
 		fakturadat *curdata;
 		int *idlist;
