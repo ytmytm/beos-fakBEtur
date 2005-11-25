4ab680ea26cf8bd83c0b73c505fb201c72a58db4
baron <baron>
2005-11-25 23:16:42 +0000

    - added bugreport about segv on idle exit (no user actions)

--- tabtowar.cpp
+++ tabtowar.cpp
@@ -7,8 +7,11 @@
 // pole 'usługa' - związane tylko z magazynem
 // kontrola wypełnienia wymaganych pól (cena, vat, itd.) - czy są i czym są
 //    (wszystko w DoCommitCurdata)
+// TODO:
 // blokada: DoCommitCurdata powinno zwracać wartość do Commit, aby móc anulować
 // zmianę pozycji (cur-dirty, zmiana na liście)
+// bug: uruchomienie i zamknięcie daje segv, wystarczy kliknąć na listę aby
+//		zamknięcie poszło czysto
 
 #include "globals.h"
 #include "tabtowar.h"
