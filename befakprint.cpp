26c4a153d5b9e2efc2f9d84ddee2f299a22b4df6
baron <baron>
2005-12-04 02:44:57 +0000

    - implemented pozfakitem table for 80col text mode - ideas for summary w/ sql

--- befakprint.cpp
+++ befakprint.cpp
@@ -1,17 +1,31 @@
 
 // TODO:
+//	- rozdzielic text od klasybazowej w miare szybko
 //	- wlaczyc slownie do klasy?
 //
 // ogolne:
 //		wygenerowanie podsumowania z rozbiciem na stawki
 //		obliczenie sumy do zaplaty
+//			- dwie kwerendy sql
+//			- jak przekazac liste wynikow do klasy potomnej? wlasna struktura?
 // text:
 //		generowanie tabelki
 //			szerokosci na sztywno dla 80/136
-//			truncate wszystko
 //		RAZEM osobno, zeby moglo zajsc na sume/slownie
+//		konfiguracja - liczba kolumn, znak konca linii
 //
 
+// podsumowanie:
+// tabela pomocnicza:
+// CREATE TEMPORARY TABLE sumawydruk ( wnetto DECIMAL(12,2), vatid INTEGER, wvat DECIMAL(12,2), wbrutto DECIMAL(12,2) );
+// wstawiac kolejne rekordy
+// INSERT INTO sumawydruk (wnetto,vatid,wvat,wbrutto) VALUES ( %Q, %i, %Q, %Q );
+// suma z rozbiciem na stawki
+// SELECT SUM(wnetto), vat/stawka, SUM(wvat), SUM(wbrutto) FROM sumawydruk GROUP BY vatid ORDER BY vatid/stawka;
+// suma calkowita
+// SELECT SUM(wnetto), vat/stawka, SUM(wvat), SUM(wbrutto) FROM sumawydruk;
+// koniec sesji
+// DROP TABLE sumawydruk;
 
 #include "befakprint.h"
 
@@ -23,6 +37,10 @@ beFakPrint::beFakPrint(int id, sqlite *db) {
 
 	dbData = db;
 	fakturaid = id;
+	typ = 0;		// XXX parametr!
+	wide = false;	// XXX parametr!
+	ncols = wide ? 136 : 80;
+
 	if (id<1) {
 		printf("illegal id!\n");
 		return;
@@ -104,6 +122,7 @@ const char *slownie(const char *input) {
 	int g = 0;		// grosze
 	int w = 0;		// wykladnik
 	bool grosze = false;
+	tmp = "";
 
 	while (i<l) {
 		c = input[i++];
@@ -155,19 +174,13 @@ const char *slownie(const char *input) {
 //----------------------
 // sample implementation
 
-#define NCOLS	80		// liczba znaków w wierszu tekstowym
 #define ELINE	"\n"	// znak końca linii
-
-// helpers
-const char *rightAlign(const BString line, const BString right);
-const char *centerAlign(const BString line);
-const char *halfAlign(const BString line, const BString right);
 
 void beFakPrint::Go(void) {
 	printf("override and do sth with data, then killyourself\n");
 // text dump
 // lewy górny: tabliczka firmy, prawy 1 wiersz: miejsce, data wydania
-BString tmp, out, line;
+BString tmp, out, line, hline;
 	out = ""; line="", tmp = "";
 	printf("--------------------\n");
 	//[1] nazwasprzedawcy .... miejscewyst,datawyst
@@ -254,6 +267,59 @@ BString tmp, out, line;
 	//[17] [wolna]
 	out += ELINE;
 	// [tabela], [podsumowanie]
+	// [naglowek]
+	if (wide) {
+	} else {
+	   hline = "+--+-------+--------+-------+----+-----+--------+--------+---+--------+--------+"; hline += ELINE;
+		out += hline;
+		line = "|  |       |        |       |    |     |        |        |   |        |        |"; line += ELINE;
+		out += line;
+		line = "|Lp| Nazwa | PKWiU  | Ilosc | Jm | Rab.| Cena z | Wartosc|VAT| Wartosc| Wartosc|"; line += ELINE;
+		out += line;
+		line = "|  |       |        |       |    | (%) |  rab.  |  netto |(%)|   VAT  | brutto |"; line += ELINE;
+		out += line;
+		out += hline;
+	}
+	// iteruj po towarach
+	pozfakitem *cur = flist->start;
+	while (cur!=NULL) {
+		line = "|";
+		if (wide) {
+		} else {
+			// lp
+			tmp = ""; tmp << cur->lp; line += fitAlignR(tmp,2); line += "|";
+			// nazwa
+			line += fitAlignL(cur->data->data[1],7); line += "|";
+			// pkwiu
+			line += fitAlignR(cur->data->data[2],8); line += "|";
+			// ilosc
+			line += fitAlignR(cur->data->data[3],7); line += "|";
+			// jm
+			line += fitAlignR(cur->data->data[4],4); line += "|";
+			// rabat
+			line += fitAlignR(cur->data->data[5],5); line += "|";
+			// cenajednostkowa
+			line += fitAlignR(cur->data->data[6],8); line += "|";
+			// w.netto
+			line += fitAlignR(cur->data->data[7],8); line += "|";
+			// vat %
+			line += fitAlignR(cur->data->data[8],3); line += "|";
+			// w.vat
+			line += fitAlignR(cur->data->data[9],8); line += "|";
+			// w.brutto
+			line += fitAlignR(cur->data->data[10],8); line += "|";
+		}
+		cur = cur->nxt;
+		line += ELINE;
+		out += line;
+	}
+	// stopka
+	if (wide) {
+	} else {
+		out += hline;	
+	}
+	// podsumowanie
+	// oblicz podsumowanie, oblicz do zaplaty
 
 	//[] [wolna]
 	out += ELINE;
@@ -285,39 +351,69 @@ BString tmp, out, line;
 	printf("---------------------\n");
 }
 
-const char *rightAlign(const BString line, const BString right) {
+const char *beFakPrint::rightAlign(const BString line, const BString right) {
 	static BString tmp;
 	int j;
 
 	tmp = line;
 	j = line.CountChars() + right.CountChars();
-	if (j < NCOLS) {
-		j = NCOLS-j;
+	if (j < ncols) {
+		j = ncols-j;
 		while (j>0) { tmp += " "; j--; };
 	}
 	tmp += right;
 	return tmp.String();
 }
 
-const char *centerAlign(const BString line) {
+const char *beFakPrint::centerAlign(const BString line) {
 	static BString tmp;
 	int j;
 
 	tmp = "";
-	j = NCOLS/2 - line.CountChars()/2;
+	j = ncols/2 - line.CountChars()/2;
 	while (j>0) { tmp += " "; j--; }
 	tmp += line;
 	return tmp.String();
 }
 
-const char *halfAlign(const BString line, const BString right) {
+const char *beFakPrint::halfAlign(const BString line, const BString right) {
 	static BString tmp;
 	int j;
 
 	tmp = line;
-	j = NCOLS/2 - line.CountChars();
+	j = ncols/2 - line.CountChars();
 	while (j>0) { tmp += " "; j--; }
 	tmp += right;
 
+	return tmp.String();
+}
+
+const char *beFakPrint::fitAlignR(const BString line, int len, bool space = false) {
+	static BString tmp;
+	int j;
+
+	if (space)
+		len--;
+
+	tmp = line;
+	tmp.Truncate(len);
+	j = tmp.CountChars();
+	if (j<len) {
+		j = len-j;
+		while (j>0) { tmp.Prepend(" "); j--; }
+	}
+	if (space)
+		tmp.Append(" ");
+	return tmp.String();
+}
+
+const char *beFakPrint::fitAlignL(const BString line, int len, bool space = false) {
+	static BString tmp;
+	if (space)
+		len--;
+	tmp = line;
+	tmp.Truncate(len);
+	if (space)
+		tmp.Prepend(" ");
 	return tmp.String();
 }
--- befakprint.h
+++ befakprint.h
@@ -13,7 +13,12 @@ class beFakPrint {
 		virtual void Go(void);
 
 		int typ;	// 0 - oryginal, 1 - kopia, 2 - duplikat
+		// tylko dla tekstowego
+		bool wide;	// tryb szerokiego druku (136) vs (80) kolumn
+		int ncols;	// liczba kolumn
+
 	private:
+		// data holders
 		fakturadat *fdata;
 		pozfaklist *flist;
 		//
@@ -22,6 +27,12 @@ class beFakPrint {
 		// db stuff
 		sqlite *dbData;
 		char *dbErrMsg;
+		// tylko dla tekstowego
+		const char *rightAlign(const BString line, const BString right);
+		const char *centerAlign(const BString line);
+		const char *halfAlign(const BString line, const BString right);
+		const char *fitAlignR(const BString line, int len, bool space = false);
+		const char *fitAlignL(const BString line, int len, bool space = false);
 };
 
 #endif
