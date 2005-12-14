97700c2227c2507fa56c42cc87ccbf2dc2ffe6a9
baron <baron>
2005-12-14 23:14:15 +0000

    - better table width selection

--- printview.cpp
+++ printview.cpp
@@ -2,9 +2,6 @@
 // - niedrukowalne 'ż'/'Ż'(ółw) zastępowane z/Z
 //
 // TODO:
-//	- truncatestring przed rysowaniem w tabelce
-//  - dynamicznie znaleźć szerokości dla tabeli - minimalne i potem align
-//	  na całą szerokość strony, zwinąć wypełnianie tabl w pętlę
 //	- info w dokumentacji jak zrobić embedding fontów w pdfwriterze
 //	- ustalić listę kilku fontów, które mają polskie literki - próbować po
 //		kolei, jak sprawdzać w BFont co zostało ustalone?
@@ -21,6 +18,10 @@
 #include <View.h>
 #include <Window.h>
 #include <stdio.h>
+
+const char *tabhline1[] = { "Lp.", "Nazwa towaru/usługi", "PKWiU", "Ilość", "J.m.", "Rabat", "Cena", "Wartość", "VAT", "Wartość", "Wartość", NULL };
+const char *tabhline2[] = { "",    "",                    "",      "",      "",     "(%)",   "z rabatem", "netto", "", "VAT", "brutto", NULL };
+const char *tabhline3[] = { "88",  "MMMMMMMMMMMMMMMMMM", "MM.MM.MM.MM", "88888.88", "MMMMM", "88.88", "88888.88", "888888.88", "88.88%", "888888.88", "888888.88", NULL };
 
 printView::printView(int id, sqlite *db, BMessage *pSettings) : beFakPrint(id,db),
 	 BView(BRect(0,0,100,100), "printView", B_FOLLOW_ALL, B_WILL_DRAW) {
@@ -72,6 +73,7 @@ printView::~printView() {
 	}
 }
 
+#define ALIGN_MARGIN	2
 #define LEFT	(pageRect.left+10)
 #define TOP		(pageRect.top+font.Size()+10)
 #define ELINE	(cur.y+font.Size()+5)
@@ -214,68 +216,49 @@ void printView::Draw(BRect pageRect) {
 	}
 	cur.y = ELINE;
 	// tabela header
+	// wygenerować szerokości
+	tabl[0] = 0;
+	float d;
+	for (i=0;i<=10;i++) {
+		d = max(font.StringWidth(tabhline1[i]), font.StringWidth(tabhline2[i]));
+		d = max(font.StringWidth(tabhline3[i]), d);
+		tabl[i+1] = tabl[i]+d;
+	}
+	float off = pageRect.Width()-tabl[11];
+	if (off>0)
+		for (i=0;i<=11;i++)
+			tabl[i] += off/2;
+//	XXX don't know how to handle it yet...
+//	else
+//		for (i=2;i<=11;i++)
+//			tabl[i] += off;
+	// finally offset everything to left margin
+	for (i=0;i<=11;i++)
+		tabl[i] += pageRect.left;
+	// paint headers...
 	MovePenTo(cur);
 	fontb.SetSize(7.0);
 	SetFont(&fontb);
-	tmp = "Lp.";
-	tabl[0] = 10;
-	tabl[1] = tabl[0]+StringWidth(tmp.String())+10;
 	cur = PenLocation();
 	cur.x = tabl[0];
 	cur.y = ELINE;
 	float tabhl0 = cur.y, tabhl1 = cur.y+fontb.Size()+1, tabhy1 = cur.y+fontb.Size()+4, tabhy0 = cur.y-(fontb.Size()+4);
-	MovePenTo(cur);
-	DrawStrCenter(tmp,tabl[0],tabl[1]);
-	tmp = "Nazwa towaru/usługi";
-	tabl[2] = tabl[1]+StringWidth(tmp.String())+65;
-	DrawStrCenter(tmp,tabl[1],tabl[2]);
-	tmp = "PKWiU";
-	tabl[3] = tabl[2]+StringWidth(tmp.String())+30;
-	DrawStrCenter(tmp,tabl[2],tabl[3]);
-	tmp = "Ilość";
-	tabl[4] = tabl[3]+StringWidth(tmp.String())+20;
-	DrawStrCenter(tmp,tabl[3],tabl[4]);
-	tmp = "J.m.";
-	tabl[5] = tabl[4]+StringWidth(tmp.String())+20;
-	DrawStrCenter(tmp,tabl[4],tabl[5]);
-	tmp = "Rabat";
-	tabl[6] = tabl[5]+StringWidth(tmp.String())+10;
-	DrawStrCenter(tmp,tabl[5],tabl[6]);
-	tmp = "(%)";
-	MovePenTo(cur.x,tabhl1);
-	DrawStrCenter(tmp,tabl[5],tabl[6]);
-	MovePenTo(cur.x,tabhl0);
-	tmp = "Cena";
-	tabl[7] = tabl[6]+StringWidth("z rabatem")+12;
-	MovePenTo(cur.x, tabhl0);
-	DrawStrCenter(tmp,tabl[6],tabl[7]);
-	tmp = "z rabatem";
-	MovePenTo(cur.x, tabhl1);
-	DrawStrCenter(tmp,tabl[6],tabl[7]);
-	MovePenTo(cur.x, tabhl0);
-	tmp = "Wartość";
-	tabl[8] = tabl[7]+StringWidth(tmp.String())+18;
-	DrawStrCenter(tmp,tabl[7],tabl[8]);
-	MovePenTo(cur.x, tabhl1);
-	tmp = "netto";
-	DrawStrCenter(tmp,tabl[7],tabl[8]);
-	MovePenTo(cur.x, tabhl0);
-	tmp = "VAT";
-	tabl[9] = tabl[8]+StringWidth(tmp.String())+14;
-	DrawStrCenter(tmp,tabl[8],tabl[9]);
-	tmp = "Wartość";
-	tabl[10] = tabl[9]+StringWidth(tmp.String())+18;
-	tabl[11] = tabl[10]+StringWidth(tmp.String())+18;
-	DrawStrCenter(tmp,tabl[9],tabl[10]);
-	DrawStrCenter(tmp,tabl[10],tabl[11]);
-	tmp = "VAT";
-	MovePenTo(cur.x, tabhl1);
-	DrawStrCenter(tmp,tabl[9],tabl[10]);
-	tmp = "brutto";
-	DrawStrCenter(tmp,tabl[10],tabl[11]);
 
+	for (i=0;i<=10;i++) {
+		cur.y = tabhl0;
+		MovePenTo(cur);
+		tmp = tabhline1[i];
+		fontb.TruncateString(&tmp, B_TRUNCATE_END, tabl[i+1]-tabl[i]);
+		DrawStrCenter(tmp, tabl[i], tabl[i+1]);
+		cur.y = tabhl1;
+		MovePenTo(cur);
+		tmp = tabhline2[i];
+		fontb.TruncateString(&tmp, B_TRUNCATE_END, tabl[i+1]-tabl[i]);
+		DrawStrCenter(tmp, tabl[i], tabl[i+1]);
+	}
 	for (i=0;i<=10;i++)
 		StrokeRect(BRect(tabl[i],tabhy0,tabl[i+1],tabhy1));
+
 	// tabela...
 	font.SetSize(8.0);
 	SetFont(&font);
@@ -283,12 +266,16 @@ void printView::Draw(BRect pageRect) {
 	// iteruj po towarach
 	pozfakitem *item = flist->start;
 	while (item!=NULL) {
-		tmp = ""; tmp << item->lp;
+		tmp = ""; tmp << item->lp; font.TruncateString(&tmp, B_TRUNCATE_END, tabl[1]-tabl[0]-ALIGN_MARGIN);
 		// lp
 		DrawStrLeft(tmp,tabl[0]);
-		DrawStrLeft(item->data->data[1], tabl[1]);
-		for (i=2;i<=10;i++)
-			DrawStrRight(item->data->data[i], tabl[i+1]);
+		tmp = item->data->data[1]; font.TruncateString(&tmp, B_TRUNCATE_END, tabl[2]-tabl[1]-ALIGN_MARGIN);
+		DrawStrLeft(tmp, tabl[1]);
+		for (i=2;i<=10;i++) {
+			tmp = item->data->data[i];
+			font.TruncateString(&tmp, B_TRUNCATE_END, tabl[i+1]-tabl[i]-ALIGN_MARGIN);
+			DrawStrRight(tmp, tabl[i+1]);
+		}
 		updateSummary(item->data->data[7].String(), item->data->vatid, item->data->data[9].String(), item->data->data[10].String());
 		item = item->nxt;
  		cur.y = ELINE; MovePenTo(cur);
@@ -300,15 +287,19 @@ void printView::Draw(BRect pageRect) {
 	makeSummary();
 	// tabela podsumowanie
 	for (i=0;i<fsummarows;i++) {
-		for (j=0;j<=3;j++)
-			DrawStrRight(fsumma[i].summa[j], tabl[8+j]);
+		for (j=0;j<=3;j++) {
+			tmp = fsumma[i].summa[j]; font.TruncateString(&tmp, B_TRUNCATE_END, tabl[8+j]-tabl[7+j]-ALIGN_MARGIN);
+			DrawStrRight(tmp, tabl[8+j]);
+		}
 		cur.y = ELINE; MovePenTo(cur);
 	}
 	StrokeLine(BPoint(tabl[7],cur.y-font.Size()-2),BPoint(tabl[11],cur.y-font.Size()-2));
 	MovePenTo(cur);
 	// razem
-	for (j=0;j<=3;j++)
-		DrawStrRight(razem.summa[j], tabl[8+j]);
+	for (j=0;j<=3;j++) {
+		tmp = razem.summa[j]; font.TruncateString(&tmp, B_TRUNCATE_END, tabl[8+j]-tabl[7+j]-ALIGN_MARGIN);
+		DrawStrRight(tmp, tabl[8+j]);
+	}
 	// RAZEM (slowo)
 	tmp = "RAZEM:";
 	DrawStrRight(tmp, tabl[7]);
@@ -323,7 +314,6 @@ void printView::Draw(BRect pageRect) {
 	EndLineArray();
 	// do zaplaty
 	cur.x = tabl[1]+(tabl[2]-tabl[1])/2;
-	cur.y = ELINE;
 	MovePenTo(cur);
 	font.SetSize(10.0);
 	tmp = "Do zapłaty zł: ";
@@ -348,7 +338,11 @@ void printView::Draw(BRect pageRect) {
 	DrawStr(tmp);
 	cur = PenLocation();
 	tmp = fdata->ogol[1];
+	font.SetSize(12.0);
+	SetFont(&font);
 	DrawStr(tmp);
+	font.SetSize(10.0);
+	SetFont(&font);
 	cur = PenLocation();
 	cur.x = pageRect.left + 1*(pageRect.Width()/8);
 	StrokeLine(BPoint(cur.x,cur.y+5),BPoint(cur.x+pageRect.Width()/4, cur.y+5));
@@ -378,14 +372,14 @@ void printView::DrawStrCenter(const BString str, float l, float r) {
 
 void printView::DrawStrLeft(const BString str, float l) {
 	BPoint cur = PenLocation();
-	cur.x = l+4;
+	cur.x = l+ALIGN_MARGIN;
 	MovePenTo(cur);
 	DrawStr(str);
 }
 
 void printView::DrawStrRight(const BString str, float r) {
 	BPoint cur = PenLocation();
-	cur.x = r-StringWidth(str.String())-4;
+	cur.x = r-StringWidth(str.String())-ALIGN_MARGIN;
 	MovePenTo(cur);
 	DrawStr(str);
 }
