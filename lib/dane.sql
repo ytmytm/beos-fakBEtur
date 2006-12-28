b5cbdd52de22f0e85e2c97368c0b2fc8cf990fdc
baron <baron>
2006-12-28 19:34:54 +0000

    - usuniecie prywatnych danych - uzupelnienie o informacje open-source

--- lib/dane.sql
+++ lib/dane.sql
@@ -1,4 +1,4 @@
-INSERT INTO firma VALUES(1,'YTM Enterprises','YTM','ul. Słowackiego 6/57','77-400','Złotów','067 265 13 18','ytm@elysium.pl','111-11-111','222-22-222','mBank','60 1140 2004 0000 3234 3268',1,1,1,0);
+INSERT INTO firma VALUES(1,'YTM Enterprises','YTM','ul. Słowackiego 0/0','77-400','Złotów','067 265 13 18','ytm@elysium.pl','111-11-111','222-22-222','mBank','60 1140 2004 0000 3234 3268',1,1,1,0);
 INSERT INTO firma VALUES(2,'Danex sp. z o.o.','DANEX','ul. Naramowicka 6/57','61-616','Poznań','058 616161','danex@danex.com.pl','','','','',0,0,1,1);
 
 INSERT INTO faktura VALUES(1,'01/11/2005','2005-11-25','Złotów','2005-11-25','2005-12-25','przelewem','własny odbiorcy','Witkowiak',NULL,1,'2005-12-12',123,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
